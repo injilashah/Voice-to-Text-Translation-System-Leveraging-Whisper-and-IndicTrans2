cab68f0ecbc7f049b120bae5a2ec4b2d26e629a1
Injila <110339290+injilashah@users.noreply.github.com>
2025-02-08 20:25:17 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,9 +15,11 @@ pip install --upgrade pip
 
 echo "Installing Python dependencies..."
 pip install --upgrade --no-deps --force-reinstall git+https://github.com/openai/whisper.git
-pip install transformers torch numpy gitpython gradio 
+pip install transformers torch numpy gitpython  
 pip install whisper  # Explicitly installing Whisper
-
+pip install bitsandbytes accelerate 
+pip install gradio
+pip install yt_dlp
 echo "Cloning IndicTrans2 repository..."
 if [ ! -d "IndicTrans2" ]; then
     git clone https://github.com/AI4Bharat/IndicTrans2
