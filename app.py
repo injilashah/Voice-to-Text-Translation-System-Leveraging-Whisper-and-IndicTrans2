73fd332dbe21e37586b7154865183fa06c76adad
Injila Shah <injilashah000@gmail.com>
2025-02-09 18:20:03 +0530

    updated readme

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from process_yt_video import download_audio,get_embed_url
 from create_srt import create_srt
 from custom_theme import CustomTheme
 
-theme = CustomTheme()
+stheme = CustomTheme()
 
 # Function to handle transcription
 def process_transcription(audio_file, youtube_url):
@@ -52,41 +52,46 @@ def update_video(youtube_url):
     embed_url = get_embed_url(youtube_url)
     return f"<iframe width='560' height='315' src='{embed_url}' frameborder='0' allowfullscreen></iframe>" if embed_url else ""
 
-# Gradio Interface
-with gr.Blocks(theme=theme )as demo:
-    gr.Markdown("<h1 style='text-align: center;'>🎙️ Voice-to-Text-Translation")
+with gr.Blocks(theme=stheme) as demo:
+    gr.Markdown("# Voice-to-Text Translation System", elem_id="title")
     
-    with gr.Row(): 
-      with gr.Column():   
-         
-            gr.Label("🎵 Transcription section",min_width=100,scale=0)
-            audio_input = gr.Audio(sources=["microphone", "upload"], type="filepath", label="Record or Upload Audio 🎤",scale=0,min_width=100)
-            youtube_url = gr.Textbox(label="Enter YouTube Link",scale=0,min_width=100)
-            video_player = gr.HTML("")
-            youtube_url.change(update_video, inputs=[youtube_url], outputs=[video_player])
+    with gr.Row():
         
-      with gr.Column():
-            transcribe_button = gr.Button("Generate Transcription", interactive=True,scale=0,min_width=300)
-            detected_language = gr.Textbox(label="Detected Language", interactive=False,scale=0,min_width=300)
-            transcription_output = gr.Textbox(label="Transcription", interactive=False,scale=0,min_width=300)
-    
-      with gr.Column():
-        gr.Label("Translation",min_width=100,scale=0)
-        language_selector = gr.Dropdown([
-            'Assamese', 'Bengali', 'Bodo', 'Dogri', 'English', 'Gujarati', 'Hindi', 'Kannada', 'Kashmiri(Perso-Arabic script)', 
-            'Kashmiri(Devanagari script)', 'Konkani', 'Maithili', 'Malayalam', 'Manipuri(Bengali script)', 
-            'Manipuri(Meitei script)', 'Marathi', 'Nepali', 'Odia', 'Punjabi', 'Sanskrit', 'Santali(Ol Chiki script)', 
-            'Sindhi(Perso-Arabic script)', 'Sindhi(Devanagari script)', 'Tamil', 'Telugu', 'Urdu'
-        ], label="Select Target Language",min_width=100,scale=0)
-        translate_button = gr.Button("Generate Translation", interactive=True,min_width=100,scale=0)
-        translation_output = gr.Textbox(label="Translation", interactive=False,min_width=100,scale=0)
-    
-      with gr.Column():
-        gr.Label("Subtitle Generation",min_width=100,scale=0)
-        subtitle_button = gr.Button("Generate Subtitles", interactive=True,min_width=100,scale=0)
-        subtitle_status = gr.Textbox(label="Subtitle Status", interactive=False,min_width=100,scale=0)
-        subtitle_download = gr.File(label="Download Subtitles", visible=True,min_width=100,scale=0)
-    
+        with gr.Column():
+          gr.Markdown("## Upload Audio or Enter YT URL")
+          audio_input = gr.Audio(sources=["upload","microphone" ], type="filepath", label="Record or Upload Audio 🎤", min_width=50)
+          youtube_url = gr.Textbox(label="Enter YouTube Link", min_width=50)
+          video_player = gr.HTML("")
+          youtube_url.change(update_video, inputs=[youtube_url], outputs=[video_player])
+        
+        with gr.Column():
+          with gr.Row():
+            gr.Markdown("" )
+            transcribe_button = gr.Button("Generate Transcription", interactive=True,size= "sm", min_width=800)
+            detected_language = gr.Textbox(label="Detected Language", interactive=False, min_width=400)
+            transcription_output = gr.Textbox(label="Transcription", interactive=False, min_width=400)
+        
+          with gr.Row():
+            language_selector = gr.Dropdown([
+                'Assamese', 'Bengali', 'Bodo', 'Dogri', 'English', 'Gujarati', 'Hindi', 'Kannada', 'Kashmiri(Perso-Arabic script)', 
+                'Kashmiri(Devanagari script)', 'Konkani', 'Maithili', 'Malayalam', 'Manipuri(Bengali script)', 
+                'Manipuri(Meitei script)', 'Marathi', 'Nepali', 'Odia', 'Punjabi', 'Sanskrit', 'Santali(Ol Chiki script)', 
+                'Sindhi(Perso-Arabic script)', 'Sindhi(Devanagari script)', 'Tamil', 'Telugu', 'Urdu'
+            ], label="Select Target Language", min_width=400)
+          with gr.Row():
+            translate_button = gr.Button("Generate Translation", interactive=True, size= "sm",min_width=350)
+          with gr.Row():
+            translation_output = gr.Textbox(label="Translation", interactive=False, min_width=400)
+        
+          with gr.Row():
+            subtitle_button = gr.Button("Generate Subtitles", interactive=True,size= "sm",min_width=350)
+          with gr.Row():
+            subtitle_status = gr.Textbox(label="Subtitle Status", interactive=False, min_width=400)
+            subtitle_download = gr.File(label="Download Subtitles", visible=True, min_width=400)
+
+
+
+
     '''with gr.Column():
         transliterate_button = gr.Button("Generate Transliteration", interactive=True)
         transliteration_output = gr.Textbox(label="Transliteration", interactive=False)'''
@@ -114,7 +119,26 @@ with gr.Blocks(theme=theme )as demo:
         inputs=[translation_output], 
         outputs=[transliteration_output]
     )'''
+# Add CSS for custom styling
+demo.css = """
+#title {
+    text-align: center;
+    font-size: 36px;
+    font-weight: bold;
+    width: 100%;
+}
 
+#label-center {
+    text-align: center;
+    font-size: 18px;
+    font-weight: bold;
+    width: 50%;
+    margin: auto;
+
+}
+
+
+"""
 # Launch the Gradio Apps
 if __name__ == "__main__":
     demo.launch(share=True, debug=True,pwa=True)
